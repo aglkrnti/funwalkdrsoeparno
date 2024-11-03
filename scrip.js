dcbf4209104387367e94c26540d2be6247a85702
aglkrnti <agilkaruniati@gmail.com>
2024-11-03 14:00:16 +0700

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,6 @@
+document.getElementById("registerBtn").addEventListener("click", function () {
+  alert("Terima kasih telah mendaftar untuk Alfamart Run 2024!");
+});
+document.getElementById("galleryImage").addEventListener("click", function () {
+  alert("Anda telah mengklik gambar!");
+});
